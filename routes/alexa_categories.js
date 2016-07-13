3c2db658680c9b3d311a0ab4b76cebce901c7bc7
BICC <bicc@LC004W136.local>
2016-07-13 19:17:06 +0800

    change phatomjs to webdriver

--- routes/alexa_categories.js
+++ routes/alexa_categories.js
@@ -13,6 +13,14 @@ const jquery = fs.readFileSync(__dirname+"/jquery.js", "utf-8");
 const exec_js_path = path.join(__dirname, '/alexa.js');
 const csv = require("fast-csv");
  
+const webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+
+const driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
 
 /*
 util.getDateString
@@ -49,12 +57,36 @@ function extractInfo(in_file,data){
 /* 根據資料時間, 進行*/
 function extractData(href,time_string,company_name){
   return new Promise(function(resolve){
+    driver.get(href);
+    driver.getPageSource()
+    .then(function(source){
+      jsdom.env({
+          "html": source,
+          "src": [jquery],
+          "done": function(error,window){
+              var $ = window.$;                   
+              /* 抓取分類 */
+              var categories  = $("#category_link_table tr[class=' ']").text().trim().split("\n").map(function(d){ return d.trim()}).join(' | ');          
+              console.log(categories);
+              resolve(categories);
+          }
+        });
+    });
+
+
+  });
+
+
+
+
+  /*
+  return new Promise(function(resolve){
     var file_filepath = __dirname+"/../rawdata/"+company_name+"_"+time_string+".html";
     var childArgs = [
       exec_js_path,
       [href,file_filepath].join(",")
     ];
-    /* 用phantomjs 將網頁資料讀取下來 */
+    // 用phantomjs 將網頁資料讀取下來 
     childProcess.execFile(phantomjs.path, childArgs, function(err, stdout, stderr) {
       if(err){
         console.log("stderr:" + stderr);
@@ -63,21 +95,19 @@ function extractData(href,time_string,company_name){
       resolve(file_filepath);
     });
   });
+  */
 }
 
 /* 執行程式 */
 function extract(records,i,time_string,f){
   /* 根據連結抓出網頁檔案放入本機，並回傳本機路徑 */
   return extractData(records[i].href,time_string,records[i].title)
-  .then(function(filepath){
-    /* 根據連結抓出關鍵資訊,回傳重點結果 */
-    return extractInfo(filepath,records[i]);
-  }).then(function(data){
+  .then(function(data){
     console.log("total",records.length);
     console.log("current",i);    
     var d = records[i];
     console.log("data",d);
-    fs.writeSync(f,[d.count,d.title,d.href,data.categories,d.description].join(",")+"\n");
+    fs.writeSync(f,[d.count,d.title,d.href,data,d.description].join(",")+"\n");
     i = i+1;    
     console.log("next",i);
     //決定結束點
@@ -92,23 +122,10 @@ function extract(records,i,time_string,f){
 function run(records,time_string){
   var f = fs.openSync(__dirname+"/../data/alexa_categories_"+time_string+".csv","w+");
   fs.writeSync(f,"count,title,href,categories,description\n");
+  records.length=5;
   extract(records,0,time_string,f);
 }
 
-/*
-function run(records,time_string){
-  var tasks = records.map(function(d){
-    return extract(d,time_string);
-  });
-
-  Promise.all(tasks).then(function(data){
-    data = data.sort(function(a,b){
-      return a.count - b.count;
-    });        
-    util.writeToCSV(data, __dirname+"/../data/alexa_categories_"+time_string+".csv");
-  });
-}
-*/
 
 var time_string = util.getDateString(new Date());
 
